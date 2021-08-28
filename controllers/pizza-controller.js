52ddd7811cfdb1cc6fc971e8cfdec6d10278d5ca
Sam Morgen <samorgen14@gmail.com>
2021-08-27 19:15:38 -0700

    validators

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -48,7 +48,10 @@ const pizzaController = {
   // update pizza by id
   updatePizza({ params, body }, res) {
     // the {new: true} makes the method return the new version of the document instead of the old
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
       .then((dbPizzaData) => {
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id!" });
